2a693dcd7dc5482498589772cdab145b6f61664d
Satyajeet Awati <93773675+SatyaA26@users.noreply.github.com>
2023-12-20 04:04:50 +0000

    New.js

--- src/Components/New.js
+++ src/Components/New.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react';
 
+const New = ({ Text }) => {
+    // 'useState' to manage the state of the 'text' variable
+    const [text, setText] = useState(Text); 
+    
+    // 'handleClick' that updates the 'text' state to 'world'
+    const handleClick = () => {             
+        setText('world');
+    };   
+
+    //'span' element with an 'onClick' event that triggers 'handleClick'
+    return <span onClick={handleClick}>{text}</span>; 
+};
+
+export default New;
