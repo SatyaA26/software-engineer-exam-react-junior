4c8a8df696ea40c8a3dd2b74af85690dc36831d0
Satyajeet Awati <93773675+SatyaA26@users.noreply.github.com>
2023-12-20 04:01:06 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import New from './Components/New'; //Newly created component
 
 const fields = [
     { name: "field_name", label: "Name", type: "text" },
@@ -8,21 +9,31 @@ const fields = [
 ];
 
 class App extends React.Component {
-
-    render(){
-
+    render() {
         return (
-        
-<div>
+            <div>
+                <h1>Webtrends Optimize Software Engineer exam</h1>
+                <form>
+                    {fields.map((field) => (
+                        <div key={field.name}>
+                            {/* 'label' element associated with the input using 'htmlFor' attribute */}
+                            <label htmlFor={field.name}>{field.label}</label>
+                            
+                            {/* 'input' element with attributes set based on the current 'field' object */}
+                            <input
+                                type={field.type}
+                                id={field.name}
+                                name={field.name}
+                            />
+                        </div>
+                    ))}
+                </form>
 
-    <h1>Webtrends Optimize Software Engineer exam</h1>
-    <form></form>
-</div>
-            
+                {/* Pass the initial text "hello" to the New component */}
+                <New Text="hello" />
+            </div>
         );
-
     }
-
 }
 
 export default App;
